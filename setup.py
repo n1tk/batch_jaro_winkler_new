4c4af7149980b99680d211f11e74c1b604be0f19
SB <sbuciuma@students.kennesaw.edu>
2021-08-02 09:29:23 -0400

    test

--- setup.py
+++ setup.py
@@ -1,24 +1,39 @@
+#!/usr/bin/python3
+
+import os
+from setuptools import setup, find_packages, Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
 
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
 
-setup(
-  name='batch_jaro_winkler',
-  version='0.1.1',
-  description='Fast batch jaro winkler distance implementation in C99.',
-  long_description='This project gets its performance from the pre-calculation of an optimized model in advance of the actual runtime calculations. Supports any encoding.',
-  author='Dominik Bousquet',
-  author_email='bousquet.dominik@gmail.com',
-  url='https://github.com/dbousque/batch_jaro_winkler',
-  license='MIT',
-  # I know, doesn't work but I don't want to use setuptools. Won't compile if < 3.3 anyway.
-  #python_requires='>=3.3',
-  setup_requires=[
-    'python>=3.6',
-    'cython>=0.28.4',
-  ],
-  #ext_modules=[Extension('batch_jaro_winkler', sources=['src/cbatch_jaro_winkler.pyx'])]
-  ext_modules=cythonize('src/cbatch_jaro_winkler.pyx', compiler_directives={"language_level": "3"})
-)
+
+extensions = [
+    Extension("batch_jaro_winkler", ["src/cbatch_jaro_winkler.pyx"]),
+]
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
