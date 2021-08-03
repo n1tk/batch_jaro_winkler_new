b4d8b3e151d7a7ad6d6613d47e4d28be173d4ea1
Sergiu Buciumas <sbuciuma@students.kennesaw.edu>
2021-08-03 11:25:44 -0400

    push code

--- setup.py
+++ setup.py
@@ -12,7 +12,10 @@ setup(
   url='https://github.com/dbousque/batch_jaro_winkler',
   license='MIT',
   # I know, doesn't work but I don't want to use setuptools. Won't compile if < 3.3 anyway.
-  install_requires=["setuptools", "wheel", "Cython",'python_version >=3.6'
-  ],
+  install_requires=["setuptools", "wheel", "Cython"],
+  python_requires='>=3.6',
+  zip_safe = False,
+  include_package_data = True,
+  package_dir= {'': 'src'},
   ext_modules=[Extension('batch_jaro_winkler', ['src/cbatch_jaro_winkler.c', 'src/ext/batch_jaro_winkler.c'], language='c')]
 )
