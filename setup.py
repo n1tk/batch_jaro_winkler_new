5be946447c14576de87a01a403cc91cc3bdc8d93
n1tk <sbuciuma@students.kennesaw.edu>
2021-08-03 15:30:46 -0400

    push

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
   zip_safe = False,
   include_package_data = True,
   package_dir= {'': 'src'},
-  ext_modules=cythonize("cbatch_jaro_winkler.pyx"),
+  ext_modules=cythonize("src/cbatch_jaro_winkler.pyx"),
   #ext_modules=[Extension('batch_jaro_winkler', ['src/cbatch_jaro_winkler.c', 'src/lib/batch_jaro_winkler.c'], language='c')]
 )
