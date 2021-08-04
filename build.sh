f7a6c9891c03bdd57a1b22f251f7c52b7265d87b
Sergiu Buciumas <sbuciuma@students.kennesaw.edu>
2021-08-04 16:24:45 -0400

    meta

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-mkdir -vp ${PREFIX}/bin;
+$PYTHON setup.py install     # Python command to install the script.
 
-export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
-export CXXLAGS="${CFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
-
-touch requirements.txt;
-
-${PYTHON} setup.py install || exit 1;
