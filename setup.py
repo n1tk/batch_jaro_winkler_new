8cf4135d6c6a24f021b26c830a552469bbb9c2e2
Sergiu Buciumas <sbuciuma@students.kennesaw.edu>
2021-08-05 08:58:00 -0400

    push all

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup, Extension
+from setuptools import _install_setup_requires, setup, Extension
 from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 import sys
+from glob import glob
+
+#ext_modules=cythonize("src/cbatch_jaro_winkler.pyx")
+#ext_modules=[Extension('batch_jaro_winkler', ['src/cbatch_jaro_winkler.c', 'src/lib/batch_jaro_winkler.c'], language='c')]
+#['src/cbatch_jaro_winkler.pyx']
+include_dirs = ['src']
+sources = glob('src/*.pyx') + glob('src/*.c') + glob('src/lib/*.c') + glob('src/lib/*.h')
+libraries = ['dl']
 
 setup(
   name='batch_jaro_winkler',
@@ -12,11 +20,17 @@ setup(
   author_email='buciuser@gmail.com',
   url='https://github.com/n1tk/batch_jaro_winkler_new',
   license='MIT',
-  install_requires=["setuptools", "Cython"],
+  setup_requires=["setuptools", "cython"],
+  install_requires=["python >=3.6", "cython"],
   python_requires='>=3.6',
   zip_safe = False,
   include_package_data = True,
   package_dir= {'': 'src'},
-  ext_modules=cythonize("src/cbatch_jaro_winkler.pyx")
-  #ext_modules=[Extension('batch_jaro_winkler', ['src/cbatch_jaro_winkler.c', 'src/lib/batch_jaro_winkler.c'], language='c')]
+  ext_modules=[Extension('batch_jaro_winkler',
+                         sources=sources,
+                         include_dirs=include_dirs,
+                         libraries=libraries,
+                         ),
+               ],
 )
+
