5825f22dfbc310613d8c8d68cddf0b7de6b5f5c4
Sergiu Buciumas <sbuciuma@students.kennesaw.edu>
2021-08-05 09:23:26 -0400

    done

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$PYTHON setup.py install     # Python command to install the script.
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
 
